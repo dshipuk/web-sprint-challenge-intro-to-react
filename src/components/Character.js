c033b3c1ee4849818079ae97374f202f90abd3b2
unknown <dshipuk@yahoo.com>
2022-03-25 11:38:07 -0700

    changes to styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,21 +1,16 @@
 // Write your Character component here
 import React from "react";
 import Char from "./create.js";
-import styled from "styled-components";
-
-const ContainerStyle = styled.div`
-background-color: rgba(0, 0, 0, 0.5)
-`
 
 const Characters = (props) => {
 
     return (
-        <ContainerStyle className="container">
+        <div className="container">
             {props.data?.map(ele => {
                 console.log(ele)
                 return <Char info={ele} />
             })}
-        </ContainerStyle>
+        </div>
     )
 };
 
